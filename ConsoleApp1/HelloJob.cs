d8941c3ed730401e18ceeaaed7ebd3b671236294
Jil Mehta <jilmehta52@gmail.com>
2023-06-25 22:52:57 -0400

    Initial

---
+++ ConsoleApp1/HelloJob.cs
@@ -0,0 +1,14 @@
+﻿using Quartz;
+using System.Diagnostics;
+
+public class HelloJob : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        //var now = DateTime.Now.ToString();
+        var message = $"Simple executed at {DateTime.Now.ToString()}";
+        Console.BackgroundColor
+            = ConsoleColor.Red;
+        Console.WriteLine(message);
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Quartz;
+
+var builder = Host.CreateDefaultBuilder()
+    .ConfigureServices((cxt, services) =>
+    {
+        services.AddQuartz(q =>
+        {
+            q.UseMicrosoftDependencyInjectionJobFactory();
+        });
+        services.AddQuartzHostedService(opt =>
+        {
+            opt.WaitForJobsToComplete = true;
+        });
+    }).Build();
+
+var schedulerFactory = builder.Services.GetRequiredService<ISchedulerFactory>();
+var scheduler = await schedulerFactory.GetScheduler();
+
+// define the job and tie it to our HelloJob class
+var job = JobBuilder.Create<HelloJob>()
+    .WithIdentity("myJob", "group1")
+    .Build();
+
+// Trigger the job to run now, and then every 40 seconds
+var trigger = TriggerBuilder.Create()
+    .WithIdentity("myTrigger", "group1")
+    .StartNow()
+    .WithSimpleSchedule(x => x
+        .WithIntervalInSeconds(5)
+        .RepeatForever())
+    .Build();
+
+await scheduler.ScheduleJob(job, trigger);
+
+// will block until the last running job completes
+await builder.RunAsync();
